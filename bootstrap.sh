2b2d0b920179142098a954f84baabb1e1dc204d8
Augusto César Dias <augusto.c.dias@gmail.com>
2021-09-17 16:16:58 +0200

    Adding script to bootstrap github codespaces env

---
+++ bootstrap.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+# Setup environment for github codespaces
+
+export DEBIAN_FRONTEND=noninteractive
+export INSTALL_ZSH=true
+export USERNAME=`whoami`
+
+## update and install required packages
+sudo apt-get update
+sudo apt-get -y install --no-install-recommends apt-utils dialog 2>&1
+sudo apt-get install -y \
+  curl \
+  git \
+  gnupg2 \
+  jq \
+  sudo \
+  openssh-client \
+  less \
+  iproute2 \
+  procps \
+  wget \
+  unzip \
+  apt-transport-https \
+  lsb-release 
+
+# Install Azure CLI
+# echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $(lsb_release -cs) main" | sudo tee -a /etc/apt/sources.list.d/azure-cli.list
+# curl -sL https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add - 2>/dev/null
+# sudo apt-get update
+# sudo apt-get install -y azure-cli;
+
+# Install Jetbrains Mono and Victor Mono fonts
+mkdir -p ~/.local/share/fonts
+cd ~/.local/share/fonts && curl -fLo "Victor Mono.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/VictorMono/Regular/complete/Victor%20Mono%20Regular%20Nerd%20Font%20Complete.ttf
+
+wget https://download.jetbrains.com/fonts/JetBrainsMono-2.001.zip
+sudo unzip JetBrainsMono-2.001.zip -d /usr/share/fonts
+sudo fc-cache -f -v
+
+# Install & Configure Zsh
+if [ "$INSTALL_ZSH" = "true" ]
+then
+    sudo apt-get install -y \
+    fonts-powerline \
+    zsh
+
+    cp -f ~/dotfiles/.zshrc ~/.zshrc
+    chsh -s /usr/bin/zsh $USERNAME
+    wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+    echo "source $PWD/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+fi
+
+# Cleanup
+sudo apt-get autoremove -y
+sudo apt-get autoremove -y
+sudo rm -rf /var/lib/apt/lists/*
