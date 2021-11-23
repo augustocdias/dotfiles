6b72340b46959b861cf90e5045fc8284a404e830
Augusto <augusto@flowciety.de>
2021-11-23 16:39:18 +0100

    Fix SSH_AUTH_SOCK variable

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,5 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+set -xg SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
+
 test -e {$HOME}/.iterm2_shell_integration.fish; and source {$HOME}/.iterm2_shell_integration.fish
 test -e {$HOME}/.fish_variables; and source {$HOME}/.fish_variables
