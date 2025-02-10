18e241d6f49acf409ec78381c9b5aac804785588
Augusto César Dias <augusto.c.dias@gmail.com>
2025-02-10 16:17:31 +0100

    Adding gitignore

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -13,12 +13,13 @@ return {
     config = function()
         require('mason').setup()
         require('mason-lspconfig').setup({
-            ensure_installed = { 'rust_analyzer', 'lua_ls', 'tsserver' },
+            ensure_installed = { 'rust_analyzer', 'lua_ls' },
             automatic_installation = true,
         })
         require('mason-tool-installer').setup({
             ensure_installed = {
                 'codelldb',
+                'harper_ls',
                 'black',
                 'markdownlint',
                 'shfmt',
