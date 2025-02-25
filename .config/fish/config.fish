b05b32a099677e93b89f4a0a8d6214b2ee07cfbb
Augusto César Dias <augusto.c.dias@gmail.com>
2025-02-25 09:19:20 +0100

    save config from tide into config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -15,5 +15,4 @@ set -gx PATH "$PNPM_HOME" $PATH
 # warmup so ssh will work at first try
 gpg --card-status 2>/dev/null 1>/dev/null
 
-# uv
-fish_add_path "/Users/augusto/.local/bin"
+tide configure --auto --style=Rainbow --prompt_colors='True color' --show_time='24-hour format' --rainbow_prompt_separators=Slanted --powerline_prompt_heads=Slanted --powerline_prompt_tails=Slanted --powerline_prompt_style='Two lines, character and frame' --prompt_connection=Solid --powerline_right_prompt_frame=No --prompt_connection_andor_frame_color=Darkest --prompt_spacing=Sparse --icons='Many icons' --transient=Yes
