d929e2f4cd8b00fde758b0a8958f2e3eb6732700
Augusto César Dias <augusto.c.dias@gmail.com>
2026-02-10 20:49:37 +0100

    update macos config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -32,10 +32,3 @@ end
 
 starship init fish | source
 enable_transience
-
-# pnpm
-set -gx PNPM_HOME /Users/augusto/Library/pnpm
-if not string match -q -- $PNPM_HOME $PATH
-    set -gx PATH "$PNPM_HOME" $PATH
-end
-# pnpm end
