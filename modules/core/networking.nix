ae8d3a2edba5ff162583f7dc51bd94fbb30b13eb
Augusto César Dias <augusto.c.dias@gmail.com>
2026-02-11 11:24:47 +0100

    make startup ignore online status

--- modules/core/networking.nix
+++ modules/core/networking.nix
@@ -4,6 +4,9 @@
   networking.networkmanager.enable = true;
   services.resolved.enable = true;
 
+  # Disable wait-online (causes 60s boot delay via docker → network-online.target)
+  systemd.services.NetworkManager-wait-online.enable = false;
+
   # Bluetooth
   hardware.bluetooth.enable = true;
   services.blueman.enable = true;
