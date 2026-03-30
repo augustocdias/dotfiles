756845b602c2f3194cd8cd45156e85f121db7e23
Augusto César Dias <augusto.c.dias@gmail.com>
2026-03-30 11:45:11 +0200

    update system and add cachix support

--- modules/core/nix.nix
+++ modules/core/nix.nix
@@ -4,7 +4,17 @@
       nixpkgs.config.allowUnfree = true;
 
       nix = {
-        settings.experimental-features = ["nix-command" "flakes"];
+        settings = {
+          experimental-features = ["nix-command" "flakes"];
+          extra-substituters = [
+            "https://hyprland.cachix.org"
+            "https://nix-community.cachix.org"
+          ];
+          extra-trusted-public-keys = [
+            "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+            "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+          ];
+        };
         gc = {
           automatic = true;
           dates = "weekly";
