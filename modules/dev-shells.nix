cbd630a2592a718ba2022934087cd03084a44e24
Augusto César Dias <augusto.c.dias@gmail.com>
2026-03-30 11:59:27 +0200

    add neovim dev-shell for plugin dev

---
+++ modules/dev-shells.nix
@@ -0,0 +1,22 @@
+{ inputs, ... }:
+{
+  perSystem = { pkgs, ... }:
+  let
+    neovimPkg = inputs.self.nixosConfigurations.laptop.config.home-manager.users.augusto.programs.neovim.finalPackage;
+  in
+  {
+    devShells.nvim-dev = pkgs.mkShell {
+      packages = [
+        (pkgs.writeShellScriptBin "nvim" ''
+          PLUGIN_DIR="''${NVIM_DEV_PLUGIN:-$PWD}"
+          exec ${neovimPkg}/bin/nvim --cmd "lua vim.opt.rtp:prepend('$PLUGIN_DIR')" "$@"
+        '')
+      ];
+
+      shellHook = ''
+        echo "Neovim dev shell active"
+        echo "Plugin dir: ''${NVIM_DEV_PLUGIN:-$PWD}"
+      '';
+    };
+  };
+}
